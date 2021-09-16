d2edd144af7ecf21fa04fefa38bc1059b8140319
suhrobcoder <suhrobcoder@gmail.com>
2021-09-16 14:06:44 +0500

    database

---
+++ lib/data/database/database_helper.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static const _dbName = "math_game.db";
+  static const _dbVersion = 1;
+
+  DatabaseHelper._privateConstructor();
+
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    _database ??= await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), _dbName);
+    await _copyDatabase(path);
+    return await openDatabase(path, version: _dbVersion);
+  }
+
+  Future<void> _copyDatabase(String path) async {
+    var exists = await databaseExists(path);
+    if (!exists) {
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {}
+      ByteData data = await rootBundle.load("assets/database/math_game.db");
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(path).writeAsBytes(bytes, flush: true);
+    }
+  }
+}
