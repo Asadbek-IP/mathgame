e51d375a44c06bcd588ca7c198b353d1880a8da6
suhrobcoder <suhrobcoder@gmail.com>
2021-09-18 18:51:29 +0500

    repository

---
+++ lib/data/repository/game_repository.dart
@@ -0,0 +1,25 @@
+import 'package:mathgame/data/database/database_helper.dart';
+import 'package:mathgame/data/models/difficulty.dart';
+import 'package:mathgame/data/models/level.dart';
+import 'package:mathgame/data/models/question.dart';
+import 'package:mathgame/data/models/world.dart';
+import 'package:mathgame/util/constants.dart';
+
+class GameRepository {
+  final DatabaseHelper dbHelper;
+
+  GameRepository(this.dbHelper);
+
+  Stream<int> changeStream() => dbHelper.changeStream;
+
+  Future<List<Difficulty>> getAllDifficulties() => dbHelper.getAllDifficulties();
+
+  Future<List<World>> getWorldsByDifficulty(int difficultyId) =>
+      dbHelper.getWorldsByDifficulty(difficultyId);
+
+  Future<List<Level>> getLevelsByWorld(World world) => dbHelper.getLevelsByWorld(world);
+
+  List<Question> getQuestionsForWorld(World world, {int count = questionPerLevel}) {
+    return List.generate(count, (_) => Question.generateQuestion(world));
+  }
+}
