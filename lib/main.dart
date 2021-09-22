23c8a155c29dd3b29cbf1e26d8e36e5a437438be
suhrobcoder <suhrobcoder@gmail.com>
2021-09-22 19:33:30 +0500

    fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mathgame/pages/home_page.dart';
 import 'package:mathgame/pages/splash.dart';
 import 'package:mathgame/service_locator.dart';
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return MaterialApp(
       home: const Splash(),
       routes: {
